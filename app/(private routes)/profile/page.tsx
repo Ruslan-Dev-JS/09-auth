6a48d7b8c3f3c3271cc6bd97735e192ac0a3835e
Ruslan-Dev-JS <ruslynnyk0001@gmail.com>
2026-03-05 16:33:19 +0200

    Added new file

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import css from "./peofile.module.css";
+import css from "./ProfilePage.module.css";
 
 export const metadata: Metadata = {
   title: "Profile | NoteHub",
